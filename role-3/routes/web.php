02314623eebc6a5e804430c2362056d5c5b91a9f
ravindra <63799363+RavindraFunCode@users.noreply.github.com>
2022-05-21 01:47:42 +0530

    role-permission

---
+++ role-3/routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+/*Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');*/
+
+
+Route::group(['middleware'=>'auth'],function(){
+    Route::get('dashboard',[HomeController::class, 'dashboard'])->name('dashboard');
+});
+Route::group(['middleware'=>['auth','role:editor']],function(){
+    Route::get('role',function(){
+        dd('hi');
+    });
+});
+
+
+
+
+
+
+require __DIR__.'/auth.php';
+
+require __DIR__.'/auth.php';
